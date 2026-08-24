d066fdc808b3c73b1e092d1e92b5298f0b8aeb09
Cong Loi Le <congloile@gmail.com>
2026-08-24 19:23:11 +0300

    weiteres Updates

--- module_3&4/ex3_3.py
+++
@@ -1,6 +0,0 @@
-len = float(input("Please enter the length of the rectangle: "))
-wid = float(input("Please enter the width of the rectangle: "))
-perimeter = 2 * (len + wid)
-area = len * wid
-print(f"The perimeter of the rectangle is: {perimeter}")
-print(f"The area of the rectangle is: {area}")
---
+++ module_4/ex4_1.py
@@ -0,0 +1,5 @@
+length = float(input("How length is the zander in cm? "))
+limit = 42 - length
+if length < 42:
+    print(f"Please  release the fish back into the lake. The fish is {limit} cm below the size limit caught fish.")
+
