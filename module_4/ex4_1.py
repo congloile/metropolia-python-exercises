d92b77b81717bd1b8950ffd8ced7ca6745c1e3f3
Cong Loi Le <congloile@gmail.com>
2026-08-28 14:17:22 +0300

    fix module 4 ex 1

--- module_4/ex4_1.py
+++ module_4/ex4_1.py
@@ -1,5 +1,7 @@
-length = float(input("How length is the zander in cm? "))
+length = float(input("How long is the zander in cm? "))
 limit = 42 - length
 if length < 42:
     print(f"Please  release the fish back into the lake. The fish is {limit} cm below the size limit caught fish.")
+else:
+    print("The fish meets the size limit. You can keep the fish.")
 
