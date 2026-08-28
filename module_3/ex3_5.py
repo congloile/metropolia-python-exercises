f4c12abe0820ded55082e92102974e890606f97a
Cong Loi Le <congloile@gmail.com>
2026-08-28 14:00:06 +0300

    Fix Module 3 Ex5

--- module_3/ex3_5.py
+++ module_3/ex3_5.py
@@ -5,4 +5,9 @@ taltog = tal*20*32*13.3
 pdstog = pds*32*13.3
 ltstog = lts*13.3
 total = taltog + pdstog + ltstog
-print(f"The weight in modern units is: {total} grams")
+kg = total // 1000
+grams = total % 1000
+if kg == 1:
+    print(f"The weight in modern units is: {kg:.0f} kilogram and {grams:.2f} grams.")
+else:
+    print(f"The weight in modern units is: {kg:.0f} kilograms and {grams:.2f} grams.")
